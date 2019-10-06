218dc52cf6b90e795f88a4ecd3263a54419a4f17
smeikx <gthb@smeikx.com>
2019-10-07 01:13:59 +0200

    vereinfache Maus-Binding

--- init.lua
+++ init.lua
@@ -1,14 +1,17 @@
 require 'battery'
 require 'window'
+require 'ctrl_double_press'
+
 escape_app_toggle = require 'escape_app_toggle'
 mouse = require 'mouse'
 
 app_watcher = hs.application.watcher.new(
 	function(app_name, event_type, _)
 		if event_type == hs.application.watcher.activated then
-			mouse.map_to_application(app_name)
+			mouse.map_to_app(app_name)
 		end
 	end)
-	:start()
+:start()
 
 escape_app_toggle.init("Terminal")
+
--- mouse.lua
+++ mouse.lua
@@ -1,67 +1,46 @@
-local actions = {
-	mssn_cntrl = function() hs.application.launchOrFocus('Mission Control') end,
-	lft = function() hs.eventtap.keyStroke({}, 'Left') end,
-	rght = function() hs.eventtap.keyStroke({}, 'Right') end,
-	cmd_lft = function() hs.eventtap.keyStroke({'⌘'}, 'Left') end,
-	cmd_rght = function() hs.eventtap.keyStroke({'⌘'}, 'Right') end,
-	cmd_oe = function() hs.eventtap.keyStroke({'⌘'}, 'ö') end,
-	cmd_ae = function() hs.eventtap.keyStroke({'⌘'}, 'ä') end,
-	cmd_z = function() hs.eventtap.keyStroke({'⌘'}, 'z') end,
-	shft_cmd_z = function() hs.eventtap.keyStroke({'shift','⌘'}, 'z') end,
-	cmd_y = function() hs.eventtap.keyStroke({'⌘'}, 'y') end,
-	void = function()end
-}
-
-local buttons = {
-	middle = actions.mssn_cntrl,
-	back = actions.cmd_lft,
-	forwards = actions.cmd_rght
-}
-
 local mappings = {
-	Blender = {
-		middle = actions.void,
-		back = actions.void,
-		forwards = actions.void },
-	Safari = {
-		middle = actions.mssn_cntrl,
-		back = actions.cmd_oe,
-		forwards = actions.cmd_ae },
+	default = {
+		[3] = function() hs.eventtap.keyStroke({'⌘'}, 'Left') end,
+		[4] = function() hs.eventtap.keyStroke({'⌘'}, 'Right') end
+	},
 	Finder = {
-		middle = actions.mssn_cntrl,
-		back = actions.lft,
-		forwards = actions.rght },
+		[3] = function() hs.eventtap.keyStroke({}, 'Left') end,
+		[4] = function() hs.eventtap.keyStroke({}, 'Right') end
+	}
 }
 
-mappings.Kalender = mappings.Finder
-mappings.Finder = mappings.iBooks
-mappings.Unity = mappings.Blender
-mappings.Godot = mappings.Blender
-mappings.godot = mappings.Godot
+local mapping = {
+	[3] = mappings.default[3], -- back
+	[4] = mappings.default[4] -- forward
+}
 
-local mouse_click_event = hs.eventtap.new(
-	{hs.eventtap.event.types.otherMouseDown},
+local current_app = ''
+
+hs.eventtap.new({25}, -- otherMouseDown
 	function(event)
-		if event:getButtonState(2) then
-			buttons.middle()
-		elseif event:getButtonState(3) then
-			buttons.back()
-		elseif event:getButtonState(4) then
-			buttons.forwards()
+		local button = event:getProperty(3)
+		if button == 2 then -- middleClick
+			--[[
+			if event:getProperty(1) == 2 then -- doubleClick
+				hs.application.launchOrFocus('Mission Control')
+				return true
+			end
+			]]--
+			return current_app == 'Terminal'
+		end
+		if event:getButtonState(button) then
+			mapping[button]()
+			return true
 		end
 	end)
 :start()
 
+local E = {} -- see “Save Navigation” in ”Programming in Lua (4ed)”
 return {
-	map_to_application = function(app_name)
-		if mappings[app_name] ~= nil then
-			buttons.middle = mappings[app_name].middle
-			buttons.back = mappings[app_name].back
-			buttons.forwards = mappings[app_name].forwards
-		else
-			buttons.middle = actions.mssn_cntrl
-			buttons.back = actions.cmd_lft
-			buttons.forwards = actions.cmd_rght
-		end
+	map_to_app = function(app_name)
+		current_app = app_name
+		mapping[3] = (mappings[app_name] or E)[3] or mappings.default[3]
+		mapping[4] = (mappings[app_name] or E)[4] or mappings.default[4]
 	end
 }
+
